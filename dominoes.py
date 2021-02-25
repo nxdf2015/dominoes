9e072ac7a36ded7a7332d49324a846c186460c61
xavier niquet <niquet_xavier2002@yahoo.fr>
2021-02-25 16:44:18 +0100

    stage 3

--- dominoes.py
+++ dominoes.py
@@ -1,8 +1,6 @@
-
 import random
 
-dominos = [ [i,j] for i in range(0,7) for j in range(i, 7)]
-
+dominos = [[i, j] for i in range(0, 7) for j in range(i, 7)]
 
 
 def init():
@@ -12,32 +10,32 @@ def init():
     stock = dominos[14:]
     snake = []
     return {
-        "player" : player,
-        "computer" : computer,
-        "stock" : stock,
-        "snake" : snake
+        "player": player,
+        "computer": computer,
+        "stock": stock,
+        "snake": snake
     }
 
+
 def find_max_double(pieces):
-    doubles = [ c for c in pieces if c[0] == c[1]]
+    doubles = [c for c in pieces if c[0] == c[1]]
     if len(doubles):
         return max(doubles)
     else:
         return False
 
-def remove_piece(pieces,piece_remove):
+
+def remove_piece(pieces, piece_remove):
     return [p for p in pieces if not p == piece_remove]
 
 
-
-def update_game(game,player,value):
+def update_game(game, player, value):
     game["snake"].append(max_double)
-    game.update({ player : remove_piece(game.get(player), max_double)})
+    game.update({player: remove_piece(game.get(player), max_double)})
     return game
 
 
-
-def show(game,player):
+def show(game, player):
     return f"""Stock pieces: {game["stock"]}
 Computer pieces: {game["computer"]}
 Player pieces: {game["player"]}
@@ -45,17 +43,19 @@ Domino snake: {game["snake"]}
 Status: {player}
     """
 
+
 def render_piece(pieces):
-    return "\n".join([ f"{i}: {p}" for i,p in enumerate(pieces,start=1)])
+    return "\n".join([f"{i}: {p}" for i, p in enumerate(pieces, start=1)])
+
 
 def render_snake(pieces):
-    snake = [ str(p) for p in pieces ]
+    snake = [str(p) for p in pieces]
     if len(snake) > 3:
-        snake = [ snake[0] , snake[-1]]
+        snake = [snake[0], snake[-1]]
     return " ".join(snake)
 
 
-def render_message_status(status,draw, win):
+def render_message_status(status, draw, win):
     if draw:
         return "The game is over. It's a draw!"
     elif win:
@@ -68,23 +68,23 @@ def render_message_status(status,draw, win):
         return "It's your turn to make a move. Enter your command." if status == "player" else "Computer is about to make a move. Press Enter to continue..."
 
 
-
-def render_game(game,status, draw,win):
-    data = { "stock_size" : len(game["stock"]),
-      "computer_size" : len(game["computer"]),
-      "snake" : render_snake(game["snake"]),
-      "player" : render_piece(game["player"]),
-      "status" : render_message_status(status,draw,win)}
+def render_game(game, status, draw, win):
+    data = {"stock_size": len(game["stock"]),
+            "computer_size": len(game["computer"]),
+            "snake": render_snake(game["snake"]),
+            "player": render_piece(game["player"]),
+            "status": render_message_status(status, draw, win)}
     return render(**data)
 
-def render(** data):
+
+def render(**data):
     result = "=" * 70
     result += f"""
 
 Stock size: {data["stock_size"]}
 Computer pieces: {data["computer_size"]}
 
-{ data["snake"]}
+{data["snake"]}
 Your pieces:
 {data["player"]}
 
@@ -97,42 +97,41 @@ def find_max(game):
     while True:
         double_player = find_max_double(game.get("player"))
         double_computer = find_max_double(game.get("computer"))
-        if not(double_computer or double_player):
-            game=init()
+        if not (double_computer or double_player):
+            game = init()
         elif not double_player:
-            return ("computer",double_computer,game)
+            return ("computer", double_computer, game)
         elif not double_computer:
             return ("player", double_player, game)
         else:
             if double_player < double_computer:
-                return ("computer",double_computer,game)
+                return ("computer", double_computer, game)
             else:
-                 return ("player",double_player, game)
+                return ("player", double_player, game)
 
 
-
-
-
-def is_valid_id(game,player,id_text):
+def is_valid_id(game, player, id_text):
     return id_text.isdigit() and 0 <= int(id_text) <= len(game[player])
+
 
 def get_random_id(game):
     size = len(game["computer"])
     return random.randint(-size, size)
 
-def select_piece(game,  player):
-    if player == "player": # ask player to select a piece
+
+def select_piece(game, player):
+    if player == "player":  # ask player to select a piece
         while True:
             try:
-        #ask user choice
-                id =  int(input())# id valid: number and less than number piece
+                # ask user choice
+                id = int(input())  # id valid: number and less than number piece
                 if -len(game["player"]) <= id <= len(game["player"]):
                     return id
                 else:
                     print("Invalid input. Please try again.")
             except ValueError as e:
                 print("Invalid input. Please try again.")
-    else: #     else select random piece in computer_piece
+    else:  # else select random piece in computer_piece
         input()
         return get_random_id(game)
 
@@ -143,14 +142,12 @@ def game_draw(pieces):
     if first == last:
         count = sum([1 for p in pieces if first in p])
         return count == 8
-    else :
+    else:
         return False
 
 
-#initialize game dominoes player computer
+# initialize game dominoes player computer
 game = init()
-
-
 
 (current_player, max_double, game) = find_max(game)
 
@@ -158,41 +155,30 @@ game = update_game(game, current_player, max_double)
 
 current_player = "computer" if current_player == "player" else "player"
 
-
-
 win = False
 draw = False
 
-print(render_game(game, current_player,draw, win))
+print(render_game(game, current_player, draw, win))
 
-while not(win or draw): #loop game
+while not (win or draw):  # loop game
     # select a piece
     id_domino_selected = select_piece(game, current_player)
 
-
     if id_domino_selected == 0 and len(game["stock"]) > 0:
-    #remove piece from stock if id_domino == 0
-            piece = game["stock"].pop()
-            game[current_player].append(piece)
+        # remove piece from stock if id_domino == 0
+        piece = game["stock"].pop()
+        game[current_player].append(piece)
     else:
-    #remove piece from game[player] and add piece to the snake
+        # remove piece from game[player] and add piece to the snake
         piece = game[current_player].pop(abs(id_domino_selected) - 1)
         if id_domino_selected < 0:
-            game["snake"].insert(0,piece)
+            game["snake"].insert(0, piece)
         else:
             game["snake"].append(piece)
 
-
-    win =  len(game[current_player]) == 0  # current_player win
-    draw  = game_draw(game["snake"]) # game draw
+    win = len(game[current_player]) == 0  # current_player win
+    draw = game_draw(game["snake"])  # game draw
 
     if not win:
         current_player = "computer" if current_player == "player" else "player"
-    print(render_game(game, current_player,draw, win))
-
-
-
-
-
-
-
+    print(render_game(game, current_player, draw, win))
